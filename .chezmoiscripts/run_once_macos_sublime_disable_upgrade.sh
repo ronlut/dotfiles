3cbe7b28c2f59c064d34bd85397870f21bab0397
Rony Lutsky <3050627+ronlut@users.noreply.github.com>
2024-01-16 18:58:45 +0200

    disable sublime upgrade dialog

---
+++ .chezmoiscripts/run_once_macos_sublime_disable_upgrade.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Disable Sublime upgrade dialog on macOS unless it already is disabled
+
+# Run this script as sudo. Ref: https://unix.stackexchange.com/a/28794/19909
+test "$EUID" -eq 0 || exec sudo bash "$0" "$@"
+
+HOSTS_FILE=/etc/hosts
+SCRIPT_NAME=$(basename "$0")
+
+read -r -d '' ADDED_HOSTS <<-EOF	
+	\n# Added by ${SCRIPT_NAME}:
+	127.0.0.1 license.sublimehq.com
+	# End of section
+	EOF
+
+timestamp=$(date "+%Y-%m-%d-%H%M%S")
+
+if grep -q "license.sublimehq.com" "$HOSTS_FILE"; then
+	echo "Sublime upgrade dialog is already disabled."
+else
+	cp $HOSTS_FILE ${HOSTS_FILE}.${timestamp}.bak
+	echo -e "$ADDED_HOSTS" | cat $HOSTS_FILE - > ${HOSTS_FILE}.new
+	mv ${HOSTS_FILE}.new $HOSTS_FILE
+
+	echo "Sublime upgrade dialog is now disabled."
+fi
